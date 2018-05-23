306a681f76b67268ab21fc624ffbc0f76ed3d6c8
nextonedaygg <nextonedaygg@163.com>
2018-05-24 01:12:21 +0800

    add provider

--- app/src/main/java/service/nextoneday/com/aidlservice/MainActivity.java
+++ app/src/main/java/service/nextoneday/com/aidlservice/MainActivity.java
@@ -3,14 +3,21 @@ package service.nextoneday.com.aidlservice;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.database.ContentObserver;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Handler;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Observable;
 import java.util.Observer;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,20 +28,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mUri = Uri.parse("content://service.nextoneday.com.aidlservice.hmd.provider/vartable");
-        ContentValues values = new ContentValues();
+        Uri uri = ContactsContract.Contacts.CONTENT_URI;
+
+        ConcurrentHashMap
+
+        //获取ContentResolver
+        ContentResolver contentResolver = getContentResolver();
+        //查询数据，返回Cursor
+        Cursor cursor = contentResolver.query(uri, null, null, null, null);
+        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
 
 
-        values.put("time",System.currentTimeMillis());
-        values.put("packageName",getPackageName());
-        values.put("key","444");
-        values.put("value","我是数据");
-
-        getContentResolver().insert(mUri,values);
-
-        observer();
     }
-
     private Uri mUri;
     private ContentResolver mContentResolver;
     private HMLContentObserver mObserver;
